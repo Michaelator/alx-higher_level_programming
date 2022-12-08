19041d5fa309707eccdc76d92e4fb100dd89f314
Ifeanyi Nwosu <nwosuifeanyi9@gmail.com>
2022-12-08 00:31:29 -0800

    roma

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,40 +1,14 @@
 #!/usr/bin/python3
-def to_subtract(list_num):
-    to_sub = 0
-    max_list = max(list_num)
-
-    for n in list_num:
-        if max_list > n:
-            to_sub += n
-
-    return (max_list - to_sub)
-
-
 def roman_to_int(roman_string):
-    if not roman_string:
-        return 0
-
-    if not isinstance(roman_string, str):
-        return 0
-
-    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    list_keys = list(rom_n.keys())
-
-    num = 0
-    last_rom = 0
-    list_num = [0]
-
-    for ch in roman_string:
-        for r_num in list_keys:
-            if r_num == ch:
-                if rom_n.get(ch) <= last_rom:
-                    num += to_subtract(list_num)
-                    list_num = [rom_n.get(ch)]
-                else:
-                    list_num.append(rom_n.get(ch))
-
-                last_rom = rom_n.get(ch)
-
-        num += to_subtract(list_num)
-
-        return (num)
+    if roman_string and type(roman_string) == str:
+       rn = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+       nun_rn = [s for s in roman_string]
+       res = 0
+       l = len(nun_rn)
+       for i in range(l):
+           if i < l - 1 and rn.get(nun_rn[i]) < rn.get(nun_rn[i + 1]):
+               res -= rn.get(nun_rn[i])
+           else:
+               res += rn.get(nun_rn[i])
+        return (res)
+    return (0)
